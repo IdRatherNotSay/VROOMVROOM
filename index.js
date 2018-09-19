b66d63abde1671e56b2a195326d8f1c366315f7a
Watashi-San <42450768+WatashiSan@users.noreply.github.com>
2018-09-18 20:13:09 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const Client = require('mpp-client-xt');
+var gClient = new Client("ws://www.multiplayerpiano.com:443");
+var defaultChannel = "lobby";
+gClient.setChannel(defaultChannel);
+gClient.start();
+var ex = 0;
+var ey = 0;
+var banned = [];
+var issweeping = false;
+setInterval(function (){},100)
+setInterval(function (){ex = ex + 5;if (ex > 100){ex = -100; ey = Math.floor(Math.random() * 100)}gClient.moveMouse(ex,ey);if (issweeping){gClient.setName('You wont ever escape me.');}else{gClient.setName('You wont ever escape me');}},100);
+setInterval(function (){if (!issweeping){gClient.say('Want to sweep with any channels? you can use b!sweep [channel name]')}},1000000)
+gClient.on('broom [b!help]',function(msg){
+   if (!banned.includes(msg.p._id)) {
+   if (msg.a.split(' ')[0] == "b!sweep") {
+     gClient.say('Sweeping to '+msg.a.split(' ').slice(1).join(' ')+' is now ready to go')
+     issweeping = true;
+     gClient.setChannel(msg.a.split(' ').slice(1).join(' '))
+     setTimeout(function(){gClient.say('Well thats sweeped too much. Bye');gClient.setChannel(defaultChannel);issweeping = false;},50000)
+   }
+   if (msg.a == "b!rules"){
+      gClient.say("1. do not spam commands. but that is too annoying")
+      gClient.say("2. after you join other channels, make sure use b!sweep [channel name you joined] in lobby")
+      setTimeout(function () {gClient.say("3. stop doing b!sweep lobby, but it is inappropriate")},10000)
+      setTimeout(function () {gClient.say("4. dont ban broom after using the command, if you do it, but it is inappropriate")},20000)
+   }
+   if (msg.a == "b!help"){
+      gClient.say("general commands: b!sweep [channel name], b!rules")
+   }
+   if (msg.a.split(' ')[0] == "b!ban" && msg.p.name == "<anonymouser>") {
+     
+     banned.push(msg.a.split(' ')[1])
+     gClient.say("sucessfully banned")
+     
+   }
+    
+   if (msg.a.split(' ')[0] == "b!default" && msg.p.name == "<anonymouser>") {
+     gClient.say('Default Channel set to '+msg.a.split(' ').slice(1).join(' '))
+     defaultChannel = msg.a.split(' ').slice(1).join(' ')
+     gClient.setChannel(defaultChannel)
+     
+   }
+   }
+   if (msg.a.startsWith("b!") && banned.includes(msg.p._id)) {
+      
+      gClient.say('well, goodbye '+msg.p.name+'. you are banned from owner')
+   }
+   
+})
