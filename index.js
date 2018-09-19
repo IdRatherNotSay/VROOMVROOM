363e314d89252e451e1dfa496a1035d0a15ea3af
Watashi-San <42450768+WatashiSan@users.noreply.github.com>
2018-09-18 20:19:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,33 +8,33 @@ var ey = 0;
 var banned = [];
 var issweeping = false;
 setInterval(function (){},100)
-setInterval(function (){ex = ex + 5;if (ex > 100){ex = -100; ey = Math.floor(Math.random() * 100)}gClient.moveMouse(ex,ey);if (issweeping){gClient.setName('You wont ever escape me.');}else{gClient.setName('You wont ever escape me');}},100);
-setInterval(function (){if (!issweeping){gClient.say('Want to sweep with any channels? you can use b!sweep [channel name]')}},1000000)
-gClient.on('broom [b!help]',function(msg){
+setInterval(function (){ex = ex + 5;if (ex > 100){ex = -100; ey = Math.floor(Math.random() * 100)}gClient.moveMouse(ex,ey);if (issweeping){gClient.setName('vroom [v!help]');}else{gClient.setName('vroom [v!help]');}},100);
+setInterval(function (){if (!issweeping){gClient.say('Want to vroom with any channels? you can use v!vroom [channel name]')}},1000000)
+gClient.on('vroom [v!help]',function(msg){
    if (!banned.includes(msg.p._id)) {
-   if (msg.a.split(' ')[0] == "b!sweep") {
-     gClient.say('Sweeping to '+msg.a.split(' ').slice(1).join(' ')+' is now ready to go')
+   if (msg.a.split(' ')[0] == "v!vroom") {
+     gClient.say('Vrooming to '+msg.a.split(' ').slice(1).join(' ')+' is now ready to go')
      issweeping = true;
      gClient.setChannel(msg.a.split(' ').slice(1).join(' '))
-     setTimeout(function(){gClient.say('Well thats sweeped too much. Bye');gClient.setChannel(defaultChannel);issweeping = false;},50000)
+     setTimeout(function(){gClient.say('Well thats vroomed too much. Bye');gClient.setChannel(defaultChannel);issweeping = false;},50000)
    }
-   if (msg.a == "b!rules"){
-      gClient.say("1. do not spam commands. but that is too annoying")
-      gClient.say("2. after you join other channels, make sure use b!sweep [channel name you joined] in lobby")
-      setTimeout(function () {gClient.say("3. stop doing b!sweep lobby, but it is inappropriate")},10000)
-      setTimeout(function () {gClient.say("4. dont ban broom after using the command, if you do it, but it is inappropriate")},20000)
+   if (msg.a == "v!rules"){
+      gClient.say("1. this")
+      gClient.say("2. is")
+      setTimeout(function () {gClient.say("very")},10000)
+      setTimeout(function () {gClient.say("broken")},20000)
    }
-   if (msg.a == "b!help"){
-      gClient.say("general commands: b!sweep [channel name], b!rules")
+   if (msg.a == "v!help"){
+      gClient.say("general commands: v!sweep [channel name], v!rules")
    }
-   if (msg.a.split(' ')[0] == "b!ban" && msg.p.name == "<anonymouser>") {
+   if (msg.a.split(' ')[0] == "v!ban" && msg.p.name == "WatashiSan") {
      
      banned.push(msg.a.split(' ')[1])
      gClient.say("sucessfully banned")
      
    }
     
-   if (msg.a.split(' ')[0] == "b!default" && msg.p.name == "<anonymouser>") {
+   if (msg.a.split(' ')[0] == "v!default" && msg.p.name == "WatashiSan") {
      gClient.say('Default Channel set to '+msg.a.split(' ').slice(1).join(' '))
      defaultChannel = msg.a.split(' ').slice(1).join(' ')
      gClient.setChannel(defaultChannel)
